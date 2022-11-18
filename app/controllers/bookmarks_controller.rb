6c09d5d6998323b0188d56930b7ce03be397b95d
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-11-18 15:12:49 +0100

    maj

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,7 @@
 class BookmarksController < ApplicationController
-
+    before_action :set_bookmark, only: :destroy
+    before_action :set_list, only: [:new, :create]
+    
     def index
         @bookmarks = Bookmark.all
     end
@@ -16,5 +18,24 @@ class BookmarksController < ApplicationController
         else
         render :new
         end
+    end
+
+    def destroy
+        @bookmark.destroy
+        redirect_to list_path(@bookmark.list)
+    end
+    
+    private
+    
+    def bookmark_params
+        params.require(:bookmark).permit(:comment, :movie_id)
+    end
+    
+    def set_bookmark
+        @bookmark = Bookmark.find(params[:id])
+    end
+    
+    def set_list
+        @list = List.find(params[:list_id])
     end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: [:show, :destroy]
+
     def index
         @lists = List.all
     end
@@ -9,5 +11,29 @@ class ListsController < ApplicationController
     
     def new
         @list = List.new
+    end
+
+    def create
+        @list = List.create(list_params)
+        if @list.save
+        redirect_to lists_path(@list)
+        else
+        render :new
+        end
+    end
+    
+    def destroy
+        @list.destroy
+        redirect_to lists_path
+    end
+    
+    private
+    
+    def set_list
+        @list = List.find(params[:id])
+    end
+    
+    def list_params
+        params.require(:list).permit(:name, :photo)
     end
 end
